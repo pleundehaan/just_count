4a10f03c0d6d7c941173aaaa6a572a3b75ee9688
pleundehaan <135601079+pleundehaan@users.noreply.github.com>
2023-11-16 10:26:33 +0100

    made with click a square function

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,5 +1,6 @@
 import just_count.square
 
+
 def main():
     print(f"The square of 5 is {just_count.square.square(5)}")
 
--- src/just_count/square.py
+++ src/just_count/square.py
@@ -1,6 +1,19 @@
-def square(x):
-    return x**2
+import click
+
+
+@click.command()
+@click.argument("number", type=float)
+# @click.option(
+#     "-n",
+#     "--number",
+#     default=1,
+#     help="Number you want to square.",
+#     show_default=True,  # show default in help
+# )
+def square(number):
+    """Calculate the square of the input number"""
+    print(f"The square of {number} is equal to {number**2}")
 
 
 if __name__ == "__main__":
-    print(f"The square of 4 is {square(4)}")
+    square()
